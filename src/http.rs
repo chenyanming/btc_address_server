61d0f58810990ea353702d7aeb99353b2afab615
chenyanming <elecming@gmail.com>
2021-08-06 17:38:03 +0800

    Genereate address and return as http body

--- src/http.rs
+++ src/http.rs
@@ -2,9 +2,42 @@ use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
 use std::convert::Infallible;
 // use std::net::SocketAddr;
+use crate::{
+    read_seed,
+    wallet::{Address, Multisig, Segwit},
+};
+use anyhow::Result;
+use std::convert::TryInto;
 
-async fn hello(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World".into()))
+async fn new_addresses(_req: Request<Body>) -> Result<Response<Body>> {
+    Ok(Response::new(
+        format!(
+            "{}\n{}",
+            Segwit::from_seed(&read_seed("seed.txt").expect("Readd seed.txt error"))?,
+            Multisig::new(
+                3,
+                3,
+                vec![
+                    hex::decode(
+                        "03d728ad6757d4784effea04d47baafa216cf474866c2d4dc99b1e8e3eb936e730"
+                    )?
+                    .try_into()
+                    .expect("slice with incorrect length"),
+                    hex::decode(
+                        "03aeb681df5ac19e449a872b9e9347f1db5a0394d2ec5caf2a9c143f86e232b0d9"
+                    )?
+                    .try_into()
+                    .expect("slice with incorrect length"),
+                    hex::decode(
+                        "02d83bba35a8022c247b645eed6f81ac41b7c1580de550e7e82c75ad63ee9ac2fd"
+                    )?
+                    .try_into()
+                    .expect("slice with incorrect length"),
+                ]
+            )?
+        )
+        .into(),
+    ))
 }
 
 pub async fn start_http_server() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -14,7 +47,7 @@ pub async fn start_http_server() -> Result<(), Box<dyn std::error::Error + Send 
         // This is the `Service` that will handle the connection.
         // `service_fn` is a helper to convert a function that
         // returns a Response into a `Service`.
-        async { Ok::<_, Infallible>(service_fn(hello)) }
+        async { Ok::<_, Infallible>(service_fn(new_addresses)) }
     });
 
     let addr = ([127, 0, 0, 1], 8080).into();
--- src/main.rs
+++ src/main.rs
@@ -19,31 +19,31 @@ async fn main() -> Result<()> {
         http::start_http_server().await;
     });
 
-    log::info!(
-        "{}",
-        Segwit::from_seed(&read_seed("seed.txt").expect("Readd seed.txt error"))
-            .expect("Create address failed)")
-    );
+    // log::info!(
+    //     "{}",
+    //     Segwit::from_seed(&read_seed("seed.txt").expect("Readd seed.txt error"))
+    //         .expect("Create address failed)")
+    // );
 
-    log::info!(
-        "{}",
-        Multisig::new(
-            3,
-            3,
-            vec![
-                hex::decode("03d728ad6757d4784effea04d47baafa216cf474866c2d4dc99b1e8e3eb936e730")?
-                    .try_into()
-                    .expect("slice with incorrect length"),
-                hex::decode("03aeb681df5ac19e449a872b9e9347f1db5a0394d2ec5caf2a9c143f86e232b0d9")?
-                    .try_into()
-                    .expect("slice with incorrect length"),
-                hex::decode("02d83bba35a8022c247b645eed6f81ac41b7c1580de550e7e82c75ad63ee9ac2fd")?
-                    .try_into()
-                    .expect("slice with incorrect length"),
-            ]
-        )
-        .expect("Create address failed)")
-    );
+    // log::info!(
+    //     "{}",
+    //     Multisig::new(
+    //         3,
+    //         3,
+    //         vec![
+    //             hex::decode("03d728ad6757d4784effea04d47baafa216cf474866c2d4dc99b1e8e3eb936e730")?
+    //                 .try_into()
+    //                 .expect("slice with incorrect length"),
+    //             hex::decode("03aeb681df5ac19e449a872b9e9347f1db5a0394d2ec5caf2a9c143f86e232b0d9")?
+    //                 .try_into()
+    //                 .expect("slice with incorrect length"),
+    //             hex::decode("02d83bba35a8022c247b645eed6f81ac41b7c1580de550e7e82c75ad63ee9ac2fd")?
+    //                 .try_into()
+    //                 .expect("slice with incorrect length"),
+    //         ]
+    //     )
+    //     .expect("Create address failed)")
+    // );
 
     loop {}
 }
