64f4151cf036d25da763ad1aebb8a13b88ccfd31
Damon Chan <damon.chan@freshup.com.hk>
2021-08-08 18:39:14 +0800

    Impl GET and POST operations
    1. GET the home page.
    2. POST seed and get the segwit address.
    3. POST mofn and get the multisig address.

--- src/http.rs
+++ src/http.rs
@@ -1,43 +1,78 @@
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
-use std::convert::Infallible;
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+// use std::convert::Infallible;
 // use std::net::SocketAddr;
-use crate::{
-    read_seed,
-    wallet::{Address, Multisig, Segwit},
-};
+use crate::wallet::{Address, MofN, Multisig, PubKey, Seed, Segwit, Wallet};
 use anyhow::Result;
 use std::convert::TryInto;
 
-async fn new_addresses(_req: Request<Body>) -> Result<Response<Body>> {
-    Ok(Response::new(
-        format!(
-            "{}\n{}",
-            Segwit::from_seed(&read_seed("seed.txt").expect("Readd seed.txt error"))?,
-            Multisig::new(
-                3,
-                3,
-                vec![
-                    hex::decode(
-                        "03d728ad6757d4784effea04d47baafa216cf474866c2d4dc99b1e8e3eb936e730"
-                    )?
-                    .try_into()
-                    .expect("slice with incorrect length"),
-                    hex::decode(
-                        "03aeb681df5ac19e449a872b9e9347f1db5a0394d2ec5caf2a9c143f86e232b0d9"
-                    )?
-                    .try_into()
-                    .expect("slice with incorrect length"),
-                    hex::decode(
-                        "02d83bba35a8022c247b645eed6f81ac41b7c1580de550e7e82c75ad63ee9ac2fd"
-                    )?
-                    .try_into()
-                    .expect("slice with incorrect length"),
-                ]
-            )?
-        )
-        .into(),
-    ))
+static NOTFOUND: &[u8] = b"Oops! Not Found";
+
+async fn router(req: Request<Body>) -> Result<Response<Body>> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(
+            format!("Welcome to bitcoin address server",).into(),
+        )),
+        (&Method::POST, "/seed") => {
+            let body = hyper::body::to_bytes(req.into_body()).await?;
+            let body = String::from_utf8(body.to_vec())?;
+            log::info!("{}", body);
+            match serde_json::from_str::<Seed>(&body) {
+                Ok(v) => {
+                    let segwit = Segwit::from_seed(&v.to_string());
+                    let wallet = Wallet::new(segwit.to_address());
+                    Ok(Response::new(
+                        serde_json::to_string(&wallet).unwrap().into(),
+                    ))
+                }
+                Err(e) => Ok(Response::new(e.to_string().into())),
+            }
+        }
+        (&Method::POST, "/mofn") => {
+            let body = hyper::body::to_bytes(req.into_body()).await?;
+            let body = String::from_utf8(body.to_vec())?;
+            log::info!("{}", body);
+            match serde_json::from_str::<MofN>(&body) {
+                Ok(v) => {
+                    let public_keys = v
+                        .public_keys
+                        .iter()
+                        .map(|key| {
+                            let key: PubKey = hex::decode(key)?
+                                .try_into()
+                                .map_err(|e| anyhow::Error::msg(format!("{:?}", e)))?;
+                            Ok(key)
+                        })
+                        .collect::<Result<Vec<PubKey>>>();
+                    match public_keys {
+                        Ok(keys) => {
+                            let multisig = Multisig::new(v.m, v.n, keys);
+                            match multisig {
+                                Ok(multisig) => {
+                                    let wallet = Wallet::new(multisig.to_address());
+                                    Ok(Response::new(
+                                        serde_json::to_string(&wallet).unwrap().into(),
+                                    ))
+                                }
+                                Err(e) => Ok(Response::new(e.to_string().into())),
+                            }
+                        }
+                        Err(e) => Ok(Response::new(e.to_string().into())),
+                    }
+                }
+                Err(e) => Ok(Response::new(e.to_string().into())),
+            }
+        }
+        _ => four_oh_four(),
+    }
+}
+
+fn four_oh_four() -> Result<Response<Body>> {
+    let body = Body::from(NOTFOUND);
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(body)
+        .map_err(|e| anyhow::Error::msg(e))
 }
 
 pub async fn start_http_server() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -47,7 +82,7 @@ pub async fn start_http_server() -> Result<(), Box<dyn std::error::Error + Send 
         // This is the `Service` that will handle the connection.
         // `service_fn` is a helper to convert a function that
         // returns a Response into a `Service`.
-        async { Ok::<_, Infallible>(service_fn(new_addresses)) }
+        async { Ok::<_, anyhow::Error>(service_fn(router)) }
     });
 
     let addr = ([127, 0, 0, 1], 8080).into();
