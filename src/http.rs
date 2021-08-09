f209656b7464c8b866053e68f619c4514cdcfced
chenyanming <elecming@gmail.com>
2021-08-09 17:26:40 +0800

    Turn /seed and /mofn path handling into functions

--- src/http.rs
+++ src/http.rs
@@ -13,58 +13,51 @@ async fn router(req: Request<Body>) -> Result<Response<Body>> {
         (&Method::GET, "/") => Ok(Response::new(
             format!("Welcome to bitcoin address server",).into(),
         )),
-        (&Method::POST, "/seed") => {
-            let body = hyper::body::to_bytes(req.into_body()).await?;
-            let body = String::from_utf8(body.to_vec())?;
-            log::info!("{}", body);
-            match serde_json::from_str::<Seed>(&body) {
-                Ok(v) => {
-                    let wallet = Segwit::seed(&v.to_string()).finalize();
-                    Ok(Response::new(
-                        serde_json::to_string(&wallet).unwrap().into(),
-                    ))
-                }
-                Err(e) => bad_request(e.to_string()),
-            }
-        }
-        (&Method::POST, "/mofn") => {
-            let body = hyper::body::to_bytes(req.into_body()).await?;
-            let body = String::from_utf8(body.to_vec())?;
-            log::info!("{}", body);
-            match serde_json::from_str::<MofN>(&body) {
-                Ok(v) => {
-                    let public_keys = v
-                        .public_keys
-                        .iter()
-                        .map(|key| {
-                            let key: PubKey = hex::decode(key)?
-                                .try_into()
-                                .map_err(|e| anyhow::Error::msg(format!("{:?}", e)))?;
-                            Ok(key)
-                        })
-                        .collect::<Result<Vec<PubKey>>>();
-                    match public_keys {
-                        Ok(keys) => {
-                            let multisig =
-                                Multisig::m(v.m).n(v.n).public_keys(keys).generate_address();
-                            match multisig {
-                                Ok(multisig) => {
-                                    let wallet = multisig.finalize();
-                                    Ok(Response::new(
-                                        serde_json::to_string(&wallet).unwrap().into(),
-                                    ))
-                                }
-                                Err(e) => bad_request(e.to_string()),
-                            }
-                        }
-                        Err(e) => bad_request(e.to_string()),
-                    }
-                }
-                Err(e) => bad_request(e.to_string()),
-            }
-        }
+        (&Method::POST, "/seed") => match post_seed(req).await {
+            Ok(response) => Ok(response),
+            Err(e) => bad_request(e.to_string()),
+        },
+        (&Method::POST, "/mofn") => match post_mofn(req).await {
+            Ok(response) => Ok(response),
+            Err(e) => bad_request(e.to_string()),
+        },
         _ => four_oh_four(),
     }
+}
+
+async fn post_seed(req: Request<Body>) -> Result<Response<Body>> {
+    let body = hyper::body::to_bytes(req.into_body()).await?;
+    let body = String::from_utf8(body.to_vec())?;
+    log::debug!("{}", body);
+    let wallet = Segwit::seed(&serde_json::from_str::<Seed>(&body)?.to_string()).finalize();
+    Ok(Response::new(
+        serde_json::to_string(&wallet).unwrap().into(),
+    ))
+}
+
+async fn post_mofn(req: Request<Body>) -> Result<Response<Body>> {
+    let body = hyper::body::to_bytes(req.into_body()).await?;
+    let body = String::from_utf8(body.to_vec())?;
+    log::debug!("{}", body);
+    let mofn = serde_json::from_str::<MofN>(&body)?;
+    let keys = mofn
+        .public_keys
+        .iter()
+        .map(|key| {
+            let key: PubKey = hex::decode(key)?
+                .try_into()
+                .map_err(|e| anyhow::Error::msg(format!("{:?}", e)))?;
+            Ok(key)
+        })
+        .collect::<Result<Vec<PubKey>>>()?;
+    let wallet = Multisig::m(mofn.m)
+        .n(mofn.n)
+        .public_keys(keys)
+        .generate_address()?
+        .finalize();
+    Ok(Response::new(
+        serde_json::to_string(&wallet).unwrap().into(),
+    ))
 }
 
 fn four_oh_four() -> Result<Response<Body>> {
@@ -76,15 +69,12 @@ fn four_oh_four() -> Result<Response<Body>> {
 }
 
 fn bad_request(v: String) -> Result<Response<Body>> {
-    let body = Body::from(serde_json::json!({
-            "error": v
-        }).to_string());
+    let body = Body::from(serde_json::json!({ "error": v }).to_string());
     Response::builder()
         .status(StatusCode::BAD_REQUEST)
         .body(body)
         .map_err(|e| anyhow::Error::msg(e))
 }
-
 
 pub async fn start_http_server() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // For every connection, we must make a `Service` to handle all
